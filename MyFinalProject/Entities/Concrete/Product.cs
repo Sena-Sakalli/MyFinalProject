e2d8d7b4b14909b3b03a1c575029eee9802b7f9f
Sena-Sakalli <senassakalli@gmail.com>
2021-02-15 03:07:27 +0300

    15.02.2021

---
+++ MyFinalProject/Entities/Concrete/Product.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //internal sadece o katmanın erişimi için kullanılır
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }     
+
+        public string ProductName { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+
+}
