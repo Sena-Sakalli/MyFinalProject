1e818dca2db8bee10476f122379ff66457a2a2cc
Sena-Sakalli <senassakalli@gmail.com>
2021-02-20 19:08:02 +0300

    20.02.2021

---
+++ MyFinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+
+        public Result(bool success, string message):this(success) //constructor
+        {
+            Message = message;
+        }
+        public Result(bool success) //overloading
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+
+        public string Message { get; } //get read only'dir bu yüzden contructorda set edilebilir.
+    }
+}
