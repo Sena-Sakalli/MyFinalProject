c802de49e284c286d13998cc90e9b5c67363fb81
Sena-Sakalli <senassakalli@gmail.com>
2021-03-01 23:55:32 +0300

    03.01.2021

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -19,6 +19,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        
         public IResult Add(Product product)
         {
             //Business code
@@ -36,10 +37,10 @@ namespace Business.Concrete
 
         public IDataResult <List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 7) 
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 23)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<Product>>( _productDal.GetAll(), Messages.ProductsListed);
         }
 
